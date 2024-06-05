c5764effcbb3656338f6f9d0c7f2b9132f1d7f98
Flyingwaves <159126400+Flyingwaves@users.noreply.github.com>
2024-06-05 16:50:00 +0800

    Update 3.4_2.cpp

--- Ch_03/3.4_2.cpp
+++ Ch_03/3.4_2.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main()
 {
-	string s1, s2;
-	cin >> s1 >> s2;
-	if (s1.size() == s2.size())	cout << "二者长度相等" << endl;
-	else if (s1.size() > s2.size())	cout << s1 << endl;
-	else
-	{
-		cout << s2 << endl;
-	}
+    string s1, s2;
+    cin >> s1 >> s2;
+    if (s1.size() == s2.size())	cout << "二者长度相等" << endl;
+    else if (s1.size() > s2.size())	cout << s1 << endl;
+    else
+    {
+        cout << s2 << endl;
+    }
 
-	return 0;
+    return 0;
 }
